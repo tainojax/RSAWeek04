7e11199e884cf3eeb0f651e4d686af7183412d9c
Eliseo Hernandez <hernandez.its@gmail.com>
2022-12-22 17:15:51 -0500

    RSA Week 04 first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Reskill Americans
+Full stack development
+Week 04
+
+Write a simple javascript function
+Basic HTML with javascript file that has a function with 3 parameters. The function inputs the 3 parameters and outputs and alert with the parameter values.
---
+++ index.js
@@ -0,0 +1,6 @@
+function userDetails(fullname, height, country) {
+    let result = "Hi " + fullname + ',\nI see you are from ' + country + '.\nYour height is ' + height;
+    console.log(result);
+    alert(result);
+}
+userDetails("Eliseo Hernandez", "5 feet 7 inches", "United States of America");
